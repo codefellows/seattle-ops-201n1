6306817397b1392822458640d06e16cbca0f3161
Djavan-N <djavan@codefellows.com>
2021-02-20 00:35:16 +0000

    ops5

---
+++ class-05/challenges/ops5.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#declare globals
+file="out.txt"
+ps=$(ps aux)
+
+#ask if you want to kill something
+display_procs() {
+    echo -e "\nDo you want manage running processes? y/n?: "
+    read -r option
+    while :
+    do
+        if [ "$option" == n ]; then
+            echo -e "\nBYEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
+            break
+        else 
+            manage_procs
+        fi
+    done
+}
+
+#kill and return
+manage_procs() {
+   
+    if [ "$option" == y ]; then
+        echo -e "$ps"
+        echo -e "\nFor reference, I've put the list of running process in $file"
+        echo - e "$ps" >> "$file"
+        echo -e "\nPlease enter the PID youw want to kill\n"
+        read -r PID
+        kill -9 "$PID"
+        
+        display_procs
+    fi
+}
+
+display_procs
